1f3dc3cefeb79ee3d93afdd019fd2900cfee89ed
killmy <1601519346@qq.com>
2021-08-05 15:04:13 +0800

    Visualization/

---
+++ Visualization/readme.md
@@ -0,0 +1,4 @@
+### 参考网站
+url:https://zhuanlan.zhihu.com/p/220403674
+### tensorbox
+tensorboard --logdir="./data/log" 打开网站
--- Visualization/visualization.py
+++ Visualization/visualization.py
@@ -1,7 +1,13 @@
 import hiddenlayer as h
+from hiddenlayer import history
+from matplotlib.pyplot import step
+from torch.nn.modules import loss
 from torch.utils import data
 from torchviz import make_dot
 from tensorboardX import SummaryWriter
+import torchvision
+import time
+# 目前只尝试了这几种可视化，后面有时间再增加，先用着
 # show model
 class HVisualizationModel():
     'simply show model'
@@ -27,6 +33,38 @@ class TVisualizationModel():
 
 # show train
 class TVisualizationTrain():
-    def __init__(self) -> None:
-        pass
+    'imcomplete code'
+    def __init__(self,path) -> None:
+        self.logger = SummaryWriter(log_dir=path)
+    def add_scalar(self,step,**kwargs)->None:
+        '添加常数'
+        for key,value in kwargs.items():
+            self.logger.add_scalar(key,value,global_step=step)
+    # def add_scalar(self,name,param,step)->None:
+    #     '添加常数'
+    #     self.logger.add_scalar(name,param,global_step=step)
+    def add_image(self,name,images,image_size,step):
+        '添加当前batch的训练图片'
+        image = torchvision.utils.make_grid(images,nrow=image_size)
+        self.logger.add_image(name,image,global_step=step)
+    def add_histogram(self,name,model,step):
+        '参数直方图'
+        model = model.cpu()
+        for name ,param in model.named_parameters():
+                #https://blog.csdn.net/moshiyaofei/article/details/90519430
+                #要先转化为cpu才能转化为numpy
+                self.logger.add_histogram(name, param.data.numpy(), global_step=step)
+        model = model.cuda()
 
+# class HVisualizationTrain():
+#     def __init__(self) -> None:
+#         self.history = h.History()
+#         self.canvas = h.Canvas()
+#     def canves_scalar(self,step,**kwargs):
+#         self.history.log(step,**kwargs)
+#         for key in kwargs:
+#             self.canvas.draw_plot(self.history[key])
+#     def canves_weight(self,step,**kwargs):
+#         self.history.log(step,**kwargs)
+#         for key in kwargs:
+#             self.canvas.draw_hist(self.history[key])
